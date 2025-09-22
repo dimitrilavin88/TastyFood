e19c651de0c599574e13671f4d3ab2ee60974fa0
dimitrilavin88 <dimitrilavin@gmail.com>
2025-09-22 13:58:23 -0600

    added home page paths to logo and Home nav option

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -8,14 +8,18 @@ const Header = () => {
   const goToMenu = () => {
     navigate('/menu');
   }
+  const goToHome = () => {
+    navigate('/');
+  }
 
   return (
       <header className="header">
       <nav className="nav">
         <div className="logo">
-          <span className="logo-text">TastyFood</span>
+          <span className="logo-text" onClick={goToHome}>TastyFood</span>
         </div>
         <ul className="nav-links">
+          <li><a onClick={goToHome}>Home</a></li>
           <li><a onClick={goToMenu}>Menu</a></li>
           <li><a href="#footer">About</a></li>
           <li><a href="#footer">Contact</a></li>
