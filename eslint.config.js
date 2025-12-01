d4b930db1a0dbdd664d09f5a4690b562f9da4e85
dimitrilavin88 <dimitrilavin@gmail.com>
2025-12-01 12:45:15 -0700

    updated config to allow program to run from root

--- eslint.config.js
+++ eslint.config.js
@@ -5,9 +5,9 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'node_modules']),
+  globalIgnores(['dist', 'node_modules', 'frontend/dist', 'frontend/node_modules']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['frontend/**/*.{js,jsx}', '**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
