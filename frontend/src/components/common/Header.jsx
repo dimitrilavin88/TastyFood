24724fc61614823387981a3522dde676fcd2fb45
dimitrilavin88 <dimitrilavin@gmail.com>
2025-12-01 11:48:48 -0700

    header change

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -30,8 +30,8 @@ const Header = () => {
         <ul className="nav-links">
           <li><a onClick={goToHome}>Home</a></li>
           <li><a onClick={goToMenu}>Menu</a></li>
-          <li><a href="#footer">About</a></li>
-          <li><a href="#footer">Contact</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact-info">Contact</a></li>
           <li>
             {user ? (
               <a className="login-btn" onClick={handleLogout}>Logout</a>
